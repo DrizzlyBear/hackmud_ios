1ac2fb1b94b34846c41e012a7e2aa0797d017a12
gubester <sean.gubelman@gmail.com>
2017-03-01 22:45:00 -0800

    initial commit xcode project with AFnetworking pod

---
+++ hackmud_ios/hackmud_ios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  hackmud_ios
+//
+//  Created by Sean Gubelman on 3/1/17.
+//  Copyright © 2017 Drizzly Bear. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ hackmud_ios/hackmud_ios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  hackmud_ios
+//
+//  Created by Sean Gubelman on 3/1/17.
+//  Copyright © 2017 Drizzly Bear. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
